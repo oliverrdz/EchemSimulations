19af11d2a6b10ea7b7d32ba66d1ececd5608ce32
Oliver Rdz <oliver.rdz@gmail.com>
2020-07-22 10:39:01 -0500

    Added license to scripts

--- RandCirc-BI_banded.py
+++ RandCirc-BI_banded.py
@@ -1,19 +1,31 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jul 22 10:07:18 2020
+#### Function that creates a potential sweep waveform
+'''
+    Copyright (C) 2020 Oliver Rodriguez
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-R - e- -> O
-Diffusion with Randless circuit
-Butler Volmer
-Backwards implicit with banded matrix
 
-Simulation time: 1.8 s
-dE = 0.0001
-dX = 2e-3
+    Created on Wed Jul 22 10:07:18 2020
+    * R - e- -> O
+    * Diffusion with Randless circuit
+    * Butler Volmer
+    * Backwards implicit with banded matrix
 
-@author: oliver
-"""
+    Simulation time: 1.8 s, with:
+    * dE = 0.0001
+    * dX = 2e-3
+
+    @author: oliverrdz
+    https://oliverrdz.xyz
+'''
 
 import numpy as np
 from scipy.linalg import solve_banded
--- plots.py
+++ plots.py
@@ -1,4 +1,19 @@
-#!/usr/bin/python3
+#### Function that creates a potential sweep waveform
+'''
+    Copyright (C) 2020 Oliver Rodriguez
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
+#### @author oliverrdz
+#### https://oliverrdz.xyz
 
 import matplotlib.pyplot as plt
 
