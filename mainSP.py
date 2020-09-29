ecbad45a6db993a7ab24e68696bf6bf5ce0d1a67
Oliver Rodriguez <oliver.rdz@gmail.com>
2020-09-28 19:30:18 -0500

    Added finite dif and backwards imp

--- mainSP.py
+++ mainSP.py
@@ -1,11 +1,25 @@
 #!/usr/bin/python
 
 from softpotato.waveform import Sweep, Step
+from softpotato.simulation import FD, BI
+import matplotlib.pyplot as plt
 
-wf1 = Sweep()
-wf2 = Step()
+## Create waveform object
+wf1 = Sweep(dE = 0.005)
 
-print(wf1.E)
-print(wf2.E)
+# Simulate
+sim_FD = FD(wf1)
+sim_BI = BI(wf1)
 
-print("Hello")
+
+print("\nSimulation finished without errors!\n")
+
+
+plt.figure(1)
+plt.plot(sim_FD.E, sim_FD.i*1e3, label="FD")
+plt.plot(sim_BI.E, sim_BI.i*1e3, label="BE")
+plt.xlabel("$E$ / V")
+plt.ylabel("$i$ / mA")
+plt.legend()
+plt.grid()
+plt.show()
