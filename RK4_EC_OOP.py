05b5ba605db0fd96ec995215733b98789f3cdeff
Oliver Rodriguez <oliver.rdz@gmail.com>
2022-02-12 11:33:00 -0600

    E mechanism is working, now I need to add C

--- RK4_EC_OOP.py
+++ RK4_EC_OOP.py
@@ -11,12 +11,15 @@ class E:
     '''
         Defines an E species
     '''
-    def __init__(self, DO=1e-5, DR=1e-5, cOb=0, cRb=1e-6, ks=1e8, alpha=0.5):
+    def __init__(self, n=1, DO=1e-5, DR=1e-5, cOb=0, cRb=1e-6, E0=0, ks=1e8, 
+                 alpha=0.5):
+        self.n = n
         self.DO = DO
         self.DR = DR
         self.DOR = DO/DR
         self.cOb = cOb
         self.cRb = cRb
+        self.E0 = E0
         self.ks = ks
         self.alpha = alpha
 
@@ -46,12 +49,13 @@ class XGrid:
     '''
         Defines the grid in space
     '''
-    def __init__(self, species, tgrid):
+    def __init__(self, species, tgrid, Ageo=1):
         self.lamb = 0.45 # For the algorithm to be stable, lamb = dT/dX^2 < 0.5
         self.Xmax = 6*np.sqrt(tgrid.nT*self.lamb) # Infinite distance
         self.dX = np.sqrt(tgrid.dT/self.lamb) # distance increment
         self.nX = int(self.Xmax/self.dX) # number of distance elements
         self.X = np.linspace(0, self.Xmax, self.nX) # Discretisation of distance
+        self.Ageo = Ageo
 
         for x in species:
             if isinstance(x, E):
@@ -75,54 +79,67 @@ class XGrid:
                 x.A[0,0] = 1 # Initial condition
             else:
                 x.CP = np.zeros([tgrid.nT, self.nX])
-            print(type(x))
 
 
 class Simulate:
     '''
     '''
     def __init__(self, species, tgrid, xgrid):
-        
-        for k in range(1,nT):
-            for x in species:
-                if isinstance(x, E):
+        self.species = species
+        self.tgrid = tgrid
+        self.xgrid = xgrid
+
+        self.join(species)
+
+    def sim(self):
+        for k in range(1, tgrid.nT):
+            for s in self.species:
+                if isinstance(s, E):
                     # Boundary condition, Butler-Volmer:
-                    CR1kb = x.CR[k-1,1]
-                    CO1kb = x.CO[k-1,1]
-                    x.CR[k,0] = (CR1kb + xgrid.dX*x.Ke*np.exp(-x.alpha*x.eps[k])*(CO1kb + 
-                                 CR1kb/x.DOR))/(1 + x.dX*x.Ke*(np.exp((1-x.alpha)*x.eps[k]+
-                                 np.exp(-x.alpha*x.eps[k])/x.DOR))
-                    x.CO[k,0] = CO1kb + (CR1kb - x.CR[k,0])/x.DOR
+                    CR1kb = s.CR[k-1,1]
+                    CO1kb = s.CO[k-1,1]
+                    s.CR[k,0] = (CR1kb + xgrid.dX*s.Ke*np.exp(-s.alpha*s.eps[k]
+                                 )*(CO1kb + CR1kb/s.DOR))/(1 + xgrid.dX*s.Ke*(
+                                 np.exp((1-s.alpha)*s.eps[k])+np.exp(
+                                 -s.alpha*s.eps[k])/s.DOR))
+                    s.CO[k,0] = CO1kb + (CR1kb - s.CR[k,0])/s.DOR
                     # Runge-Kutta 4:
-                    x.CR[k,1:-1] = RK4(CR[k-1,:], 'E')[1:-1]
-                    x.CO[k,1:-1] = RK4(CO[k-1,:], 'E')[1:-1]
+                    s.CR[k,1:-1] = self.RK4(s.CR[k-1,:], 'E', s)[1:-1]
+                    s.CO[k,1:-1] = self.RK4(s.CO[k-1,:], 'E', s)[1:-1]
+        for s in self.species:
+            if isinstance(s, E):
+                # Denormalising:
+                if s.cRb:
+                    I = -s.CR[:,2] + 4*s.CR[:,1] - 3*s.CR[:,0]
+                    D = s.DR
+                    c = s.cRb
+                else: # In case only O present in solution
+                    I = s.CO[:,2] - 4*s.CO[:,1] + 3*s.CO[:,0]
+                    D = s.DO
+                    c = s.cOb
+                self.i = s.n*F*self.xgrid.Ageo*D*c*I/(2*self.xgrid.dX*s.delta)
 
-        # Denormalising:
-        if cRb:
-            I = -CR[:,2] + 4*CR[:,1] - 3*CR[:,0]
-            D = DR
-            c = cRb
-        else: # In case only O present in solution
-            I = CO[:,2] - 4*CO[:,1] + 3*CO[:,0]
-            D = DO
-            c = cOb
-        i = n*F*Ageo*D*c*I/(2*dX*delta)
+                s.cR = s.CR*s.cRb
+                s.cO = s.CO*s.cOb
+                self.x = self.xgrid.X*s.delta
 
-        cR = CR*cRb
-        cO = CO*cOb
-        x = X*delta
+    def join(self, species):
+        for s in species:
+            print(s)
 
-    def fun(y, mech='E'):
+
+    def fun(self, y, mech, species):
         if mech == 'E':
-            return np.dot(A,y) 
-        else:
-            return np.dot(A,y) - dT*Kc*y
+            return np.dot(species.A,y) 
+        elif mech == 'EC':
+            return np.dot(species.A,y) - self.tgrid.dT*species.Kc*y
 
-    def RK4(y, mech):
-        k1 = fun(y, mech)
-        k2 = fun(y+dT*k1/2, mech)
-        k3 = fun(y+dT*k2/2, mech)
-        k4 = fun(y+dT*k3, mech)
+    def RK4(self, y, mech, A):
+        dT = self.tgrid.dT
+        k1 = self.fun(y, mech, A)
+        k2 = self.fun(y+dT*k1/2, mech, A)
+        k3 = self.fun(y+dT*k2/2, mech, A)
+        k4 = self.fun(y+dT*k3, mech, A)
         return y + (dT/6)*(k1 + 2*k2 + 2*k3 + k4)
 
            
@@ -131,9 +148,12 @@ class Simulate:
 
 if __name__ == '__main__':
     import waveforms as wf
-    t, _ = wf.sweep()
-    e = E()
+    import plots as p
+    twf, Ewf = wf.sweep()
+    e = E(ks=1e-3)
     c = C()
-    tgrid = TGrid(t)
+    tgrid = TGrid(twf, Ewf)
     xgrid = XGrid([e,c], tgrid)
-    #sim = Simulate([e,c])
+    sim = Simulate([e,c], tgrid, xgrid)
+    sim.sim()
+    p.plot(Ewf, sim.i, xlab='$E$ / V', ylab='$i$ / A')
